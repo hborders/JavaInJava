8358dc1267bd5923568d9de238dd1c6c84513e17
hborders <hborders@mail.win.org>
2010-12-30 23:25:42 -0600

    Removed hosted-only code.

--- JavaInJava/src/com/sun/max/vm/object/ArrayAccess.java
+++ JavaInJava/src/com/sun/max/vm/object/ArrayAccess.java
@@ -20,8 +20,6 @@
  */
 package com.sun.max.vm.object;
 
-import static com.sun.max.vm.MaxineVM.*;
-
 import java.lang.reflect.*;
 
 import com.sun.max.annotate.*;
@@ -53,16 +51,6 @@ public final class ArrayAccess {
      */
     @INLINE
     public static int readArrayLength(Object array) {
-        if (isHosted()) {
-            if (array.getClass().isArray()) {
-                return Array.getLength(array);
-            }
-            if (array instanceof Hybrid) {
-                final Hybrid hybrid = (Hybrid) array;
-                return hybrid.length();
-            }
-            throw FatalError.unexpected("Cannot get array length of " + array.getClass().getName() + " instance");
-        }
         return Layout.readArrayLength(Reference.fromJava(array));
     }
 
@@ -89,26 +77,12 @@ public final class ArrayAccess {
      */
     @INLINE
     public static void checkSetObject(Object array, Object value) {
-        if (isHosted()) {
-            final Class arrayClass = array.getClass();
-            final Class componentType = arrayClass.getComponentType();
-            if (value != null) {
-                if (!componentType.isInstance(value)) {
-                    Throw.arrayStoreException(array, value);
-                }
-            } else {
-                if (Word.class.isAssignableFrom(componentType)) {
-                    Throw.arrayStoreException(array, value);
-                }
-            }
-        } else {
-            if (value != null) {
-                final ClassActor arrayClassActor = ObjectAccess.readClassActor(array);
-                if (!arrayClassActor.componentClassActor().isNonNullInstance(value)) {
-                    Throw.arrayStoreException(array, value);
-                }
-            }
-        }
+	    if (value != null) {
+	        final ClassActor arrayClassActor = ObjectAccess.readClassActor(array);
+	        if (!arrayClassActor.componentClassActor().isNonNullInstance(value)) {
+	            Throw.arrayStoreException(array, value);
+	        }
+	    }
     }
 
     public static Pointer elementPointer(byte[] array, int index) {
@@ -129,15 +103,6 @@ public final class ArrayAccess {
      */
     @INLINE
     public static byte getByte(Object array, int index) {
-        if (isHosted()) {
-            if (array instanceof boolean[]) {
-                final boolean[] booleanArray = (boolean[]) array;
-                return booleanArray[index] ? (byte) 1 : (byte) 0;
-            }
-            assert array instanceof byte[];
-            final byte[] byteArray = (byte[]) array;
-            return byteArray[index];
-        }
         return Layout.getByte(Reference.fromJava(array), index);
     }
 
@@ -150,17 +115,7 @@ public final class ArrayAccess {
      */
     @INLINE
     public static void setByte(Object array, int index, byte value) {
-        if (isHosted()) {
-            if (array instanceof boolean[]) {
-                final boolean[] booleanArray = (boolean[]) array;
-                booleanArray[index] = value != 0;
-            } else {
-                final byte[] byteArray = (byte[]) array;
-                byteArray[index] = value;
-            }
-        } else {
-            Layout.setByte(Reference.fromJava(array), index, value);
-        }
+        Layout.setByte(Reference.fromJava(array), index, value);
     }
 
     /**
@@ -172,10 +127,6 @@ public final class ArrayAccess {
      */
     @INLINE
     public static boolean getBoolean(Object array, int index) {
-        if (isHosted()) {
-            final boolean[] booleanArray = (boolean[]) array;
-            return booleanArray[index];
-        }
         return Layout.getBoolean(Reference.fromJava(array), index);
     }
 
@@ -188,11 +139,6 @@ public final class ArrayAccess {
      */
     @INLINE
     public static void setBoolean(Object array, int index, boolean value) {
-        if (isHosted()) {
-            final boolean[] booleanArray = (boolean[]) array;
-            booleanArray[index] = value;
-            return;
-        }
         Layout.setBoolean(Reference.fromJava(array), index, value);
     }
 
@@ -205,10 +151,6 @@ public final class ArrayAccess {
      */
     @INLINE
     public static short getShort(Object array, int index) {
-        if (isHosted()) {
-            final short[] shortArray = (short[]) array;
-            return shortArray[index];
-        }
         return Layout.getShort(Reference.fromJava(array), index);
     }
 
@@ -221,11 +163,6 @@ public final class ArrayAccess {
      */
     @INLINE
     public static void setShort(Object array, int index, short value) {
-        if (isHosted()) {
-            final short[] shortArray = (short[]) array;
-            shortArray[index] = value;
-            return;
-        }
         Layout.setShort(Reference.fromJava(array), index, value);
     }
 
@@ -238,10 +175,6 @@ public final class ArrayAccess {
      */
     @INLINE
     public static char getChar(Object array, int index) {
-        if (isHosted()) {
-            final char[] charArray = (char[]) array;
-            return charArray[index];
-        }
         return Layout.getChar(Reference.fromJava(array), index);
     }
 
@@ -254,11 +187,6 @@ public final class ArrayAccess {
      */
     @INLINE
     public static void setChar(Object array, int index, char value) {
-        if (isHosted()) {
-            final char[] charArray = (char[]) array;
-            charArray[index] = value;
-            return;
-        }
         Layout.setChar(Reference.fromJava(array), index, value);
     }
 
@@ -271,10 +199,6 @@ public final class ArrayAccess {
      */
     @INLINE
     public static int getInt(Object array, int index) {
-        if (isHosted()) {
-            final int[] intArray = (int[]) array;
-            return intArray[index];
-        }
         return Layout.getInt(Reference.fromJava(array), index);
     }
 
@@ -287,11 +211,6 @@ public final class ArrayAccess {
      */
     @INLINE
     public static void setInt(Object array, int index, int value) {
-        if (isHosted()) {
-            final int[] intArray = (int[]) array;
-            intArray[index] = value;
-            return;
-        }
         Layout.setInt(Reference.fromJava(array), index, value);
     }
 
@@ -304,10 +223,6 @@ public final class ArrayAccess {
      */
     @INLINE
     public static float getFloat(Object array, int index) {
-        if (isHosted()) {
-            final float[] floatArray = (float[]) array;
-            return floatArray[index];
-        }
         return Layout.getFloat(Reference.fromJava(array), index);
     }
 
@@ -320,11 +235,6 @@ public final class ArrayAccess {
      */
     @INLINE
     public static void setFloat(Object array, int index, float value) {
-        if (isHosted()) {
-            final float[] floatArray = (float[]) array;
-            floatArray[index] = value;
-            return;
-        }
         Layout.setFloat(Reference.fromJava(array), index, value);
     }
 
@@ -337,10 +247,6 @@ public final class ArrayAccess {
      */
     @INLINE
     public static long getLong(Object array, int index) {
-        if (isHosted()) {
-            final long[] longArray = (long[]) array;
-            return longArray[index];
-        }
         return Layout.getLong(Reference.fromJava(array), index);
     }
 
@@ -353,11 +259,6 @@ public final class ArrayAccess {
      */
     @INLINE
     public static void setLong(Object array, int index, long value) {
-        if (isHosted()) {
-            final long[] longArray = (long[]) array;
-            longArray[index] = value;
-            return;
-        }
         Layout.setLong(Reference.fromJava(array), index, value);
     }
 
@@ -370,10 +271,6 @@ public final class ArrayAccess {
      */
     @INLINE
     public static double getDouble(Object array, int index) {
-        if (isHosted()) {
-            final double[] doubleArray = (double[]) array;
-            return doubleArray[index];
-        }
         return Layout.getDouble(Reference.fromJava(array), index);
     }
 
@@ -386,11 +283,6 @@ public final class ArrayAccess {
      */
     @INLINE
     public static void setDouble(Object array, int index, double value) {
-        if (isHosted()) {
-            final double[] doubleArray = (double[]) array;
-            doubleArray[index] = value;
-            return;
-        }
         Layout.setDouble(Reference.fromJava(array), index, value);
     }
 
@@ -403,10 +295,6 @@ public final class ArrayAccess {
      */
     @INLINE
     public static Word getWord(Object array, int index) {
-        if (isHosted()) {
-            final Word[] wordArray = (Word[]) array;
-            return WordArray.get(wordArray, index);
-        }
         return Layout.getWord(Reference.fromJava(array), index);
     }
 
@@ -419,11 +307,6 @@ public final class ArrayAccess {
      */
     @INLINE
     public static void setWord(Object array, int index, Word value) {
-        if (isHosted()) {
-            final Word[] wordArray = (Word[]) array;
-            WordArray.set(wordArray, index, value);
-            return;
-        }
         Layout.setWord(Reference.fromJava(array), index, value);
     }
 
@@ -436,10 +319,6 @@ public final class ArrayAccess {
      */
     @INLINE
     public static Object getObject(Object array, int index) {
-        if (isHosted()) {
-            final Object[] objectArray = (Object[]) array;
-            return objectArray[index];
-        }
         return Layout.getReference(Reference.fromJava(array), index).toJava();
     }
 
@@ -452,11 +331,6 @@ public final class ArrayAccess {
      */
     @INLINE
     public static void setObject(Object array, int index, Object value) {
-        if (isHosted()) {
-            final Object[] objectArray = (Object[]) array;
-            objectArray[index] = value;
-            return;
-        }
         Layout.setReference(Reference.fromJava(array), index, Reference.fromJava(value));
     }
 
